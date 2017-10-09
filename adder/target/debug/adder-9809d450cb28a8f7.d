6ad46e23c1bff68e7edab4a4aace353c023548b5
Luke Roberto <luke.roberto@zenuity.com>
2017-10-09 01:37:57 -0400

    Added the adder testing examples

---
+++ adder/target/debug/adder-9809d450cb28a8f7.d
@@ -0,0 +1,1 @@
+/home/luke/mit_workspace/rust_examples/adder/target/debug/adder-9809d450cb28a8f7: /home/luke/mit_workspace/rust_examples/adder/src/lib.rs
---
+++ adder/target/debug/libadder.d
@@ -0,0 +1,1 @@
+/home/luke/mit_workspace/rust_examples/adder/target/debug/libadder.rlib: /home/luke/mit_workspace/rust_examples/adder/src/lib.rs
